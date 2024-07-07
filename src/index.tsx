17e42a522abfa286937e47776d4f10c18fdc3e87
zBlade <dnguy3n2010@gmail.com>
2024-07-07 17:58:32 -0400

    Initialize project

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+/**
+ * @file This file is the entry point for your project.
+ */
+
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+const root = createRoot(document.getElementById("root") ?? document.body);
+root.render(<div>Hello World!</div>);
